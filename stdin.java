9793a5a301b30727a1bf5e37796acfa5aeb0700a
harshilpatel <harshil912@gmail.com>
2016-05-20 20:30:08 +0530

    just getting started

---
+++ stdin.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class stdin{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int incoming = 0;
+        for(int i = 0; i<3;i++){
+            System.out.println(s.nextInt());
+        }        
+    }
+}
