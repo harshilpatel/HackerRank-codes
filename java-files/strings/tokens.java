1db22787cd17a081b62c2c6cbc826aca4605953c
harshilpatel <harshil912@gmail.com>
2016-12-11 23:06:17 +0530

    para in java

--- java-files/strings/tokens.java
+++ java-files/strings/tokens.java
@@ -4,20 +4,16 @@ import java.util.*;
 class tokens{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String a = s.nextLine();
+        String str = s.nextLine().trim();
+        String reg = "[!,?._'@ ]+";
+        String[] tokens = str.split(reg);
         String result = "";
-        int resultTokens = 0;
+        int resultTokens = tokens.length;
+        if(str.length() == 0) resultTokens = 0;
 
-        for(int i=0; i<a.length(); i++){
-            char b = a.charAt(i);
-            if(((int)b >= 97 && (int)b <= 122) || ((int)b >= 65 && (int)b <= 90)){
-                result += b;
-            } else if(i>0 && result.charAt(result.length()-1) != '\n'){
-                    result += "\n"; 
-                    resultTokens++;
-            }
-        }
         System.out.println(resultTokens);
-        System.out.print(result);
+        for(int i=0; i<resultTokens; i++){
+          System.out.print(tokens[i] + "\n");
+        }
     }
 }
